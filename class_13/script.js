ab088b640683149b0a2a81032dd9f336825a2541
RUBYAT ISLAM <rubyat.ais@gmail.com>
2020-11-16 11:06:54 +0600

    class 13 jQuery
    class 13 jQuery

---
+++ class_13/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	
+	$("#my_add_class").click(function(){
+		$('h1').addClass('text_green');
+	});
+	
+	$("#my_remove_class").click(function(){
+		$('h1').removeClass('text_green');
+	});
+
+	
+});
