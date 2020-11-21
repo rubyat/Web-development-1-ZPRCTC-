56266df8177d489e78cc1ecc8e54aa8f6929ab19
RUBYAT ISLAM <rubyat.ais@gmail.com>
2020-11-21 11:21:08 +0600

    Class 14
    Class 14

---
+++ class_14/carousel_active/assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	
+	$('.owl-carousel').owlCarousel({
+		loop:true,
+		margin:10,
+		nav:false,
+		items:6,
+	})
+
+	
+});
---
+++ class_14/carousel_active_extend/assets/js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	
+	$('#our_clients').owlCarousel({
+		loop:true,
+		margin:10,
+		nav:false,
+		items:6,
+	});
+	
+	
+	$('#our_promo').owlCarousel({
+		loop:true,
+		margin:0,
+		nav:false,
+		items:3,
+	});
+	
+	$('#about_area').owlCarousel({
+		loop:true,
+		margin:0,
+		nav:false,
+		items:1,
+	});
+	
+	$('#service_area').owlCarousel({
+		loop:true,
+		margin:30,
+		nav:false,
+		items:3,
+	});
+
+	
+});
